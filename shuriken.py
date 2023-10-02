731c359cad41c26892deef0903a558e4fb3cbc41
quentin <brise.quentin@gmail.com>
2023-10-02 16:31:27 +0200

    facto code classe stickman

--- shuriken.py
+++ shuriken.py
@@ -3,6 +3,14 @@
 import pygame, sys
 from pygame.locals import *
 
+
+#je viens de comprendre qu'il utilisait 2 classe pour lancer un shuriken, un pour la droite et un pour la gauche.
+#c'est encore pire, cad il a une classe pour chaque direction, et chaque personnage.
+#le mieux a faire est d'abord de modifier la classe personnage afin d'obtenir une unique classe pour tout le monde.
+#
+
+
+
 class ShurikenDR(pygame.sprite.Sprite):
 
  # initialisation (en fonction de la vitesse voulue et aussi lors du passage du shuriken dans un portail)
@@ -15,6 +23,9 @@ class ShurikenDR(pygame.sprite.Sprite):
 
       #mouvement du shuriken
     def update(self):
+
+
+        #s'il sort de la map j'imagine ?
         self.rect.x+=8
         if self.rect.x>1100:
             self.kill()
@@ -51,6 +62,68 @@ class ShurikenGR(pygame.sprite.Sprite):
         if self.rect.x<-21:
             self.kill()
 
+
+        tp2=0
+        if self.rect.x<50:
+            if 300<self.rect.y<350:
+                tp2=1
+        if tp2==1:
+            self.rect.x= 1040
+            self.rect.y= 250
+        tp3=0
+        if self.rect.x<50:
+            if 120<self.rect.y<190:
+                tp3=1
+        if tp3==1:
+            self.rect.x= 1040
+            self.rect.y= 445
+
+
+class ShurikenDB(pygame.sprite.Sprite):
+ # initialisation (en fonction de la vitesse voulue et aussi lors du passage du shuriken dans un portail)
+    def __init__(self):
+        super().__init__()
+
+        self.image=pygame.image.load('ShurikenBleu1.png').convert_alpha()
+        self.rect = self.image.get_rect()
+
+      #mouvement du shuriken
+    def update(self):
+        self.rect.x+=8
+        if self.rect.x>1100:
+            self.kill()
+
+        tp0=0
+
+        if self.rect.x>1050:
+            if 400<self.rect.y<500:
+                tp0=1
+
+        if tp0==1:
+            self.rect.x = 50
+            self.rect.y = 130
+
+        tp1=0
+        if self.rect.x>1050:
+            if 200<self.rect.y<250:
+                tp1=1
+        if tp1==1:
+            self.rect.x = 50
+            self.rect.y= 330
+
+class ShurikenGB(pygame.sprite.Sprite):
+ # initialisation (en fonction de la vitesse voulue et aussi lors du passage du shuriken dans un portail)
+    def __init__(self):
+        super().__init__()
+
+        self.image=pygame.image.load('ShurikenBleu1.png').convert_alpha()
+        self.rect = self.image.get_rect()
+
+        #mvt Shuriken
+    def update(self):
+        self.rect.x-=7
+        if self.rect.x<-21:
+            self.kill()
 
         tp2=0
         if self.rect.x<50:
--- stickman.py
+++ stickman.py
@@ -3,10 +3,34 @@ from pygame.locals import *
 
 #l 15-180;254-418
 
+
+class Stickman(pygame.sprite.Sprite):
+    def __init__(self, color):
+        super().__init__()
+
+        #importer les images des persos.
+        #https://www.geeksforgeeks.org/pygame-flip-the-image/
+        #tobeused for left side
+
+        # initialisation des caracteristiques
+
+        #position
+        self.change_x =0
+        self.change_y =0
+        self.vie=4
+
+        self.color=color   #0 red, 1 blue
+        self.orientation=color #0 left, 1 right
+        
+        self.anim=0
+        self.tir=0
+        self.score=0
+
 #creation d'une classe du Bonhomme Rouge
 class Stickmanrouge(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+
         self.Run1DR=pygame.image.load("1RunDR.png").convert_alpha()
         self.Run2DR=pygame.image.load('2RunDR.png').convert_alpha()
         self.Run3DR=pygame.image.load('3RunDR.png').convert_alpha()
@@ -68,7 +92,7 @@ class Stickmanrouge(pygame.sprite.Sprite):
 
         if self.orientation==0:
             if self.change_x==0 and self.change_y==0:
-                self.image = self.baseGR
+                self.image = self.baseGR  #orientation 0 gauche rouge ?
             elif self.change_y==0 :
                 self.rect.y=455
                 if 0<self.anim and self.anim<10:
@@ -163,6 +187,173 @@ class Stickmanrouge(pygame.sprite.Sprite):
                 if self.anim>=-2:
                     self.anim=0
 
+
+
+
+
+    def stop(self):
+        """arret"""
+        self.change_x=0
+
+
+class Stickmanbleu(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.Run1DB=pygame.image.load('1RunDB.png').convert_alpha()
+        self.Run2DB=pygame.image.load('2RunDB.png').convert_alpha()
+        self.Run3DB=pygame.image.load('3RunDB.png').convert_alpha()
+        self.Run4DB=pygame.image.load('4RunDB.png').convert_alpha()
+        self.Run5DB=pygame.image.load('5RunDB.png').convert_alpha()
+        self.Run6DB=pygame.image.load('6RunDB.png').convert_alpha()
+        self.Run1GB=pygame.image.load('1RunGB.png').convert_alpha()
+        self.Run2GB=pygame.image.load('2RunGB.png').convert_alpha()
+        self.Run3GB=pygame.image.load('3RunGB.png').convert_alpha()
+        self.Run4GB=pygame.image.load('4RunGB.png').convert_alpha()
+        self.Run5GB=pygame.image.load('5RunGB.png').convert_alpha()
+        self.Run6GB=pygame.image.load('6RunGB.png').convert_alpha()
+        self.baseGB=pygame.image.load('deboutGB.png').convert_alpha()
+        self.baseDB=pygame.image.load('deboutDB.png').convert_alpha()
+        self.Jump1DB=pygame.image.load('1JumpDB.png').convert_alpha()
+        self.Jump2DB=pygame.image.load('2JumpDB.png').convert_alpha()
+        self.Jump1GB=pygame.image.load('1JumpGB.png').convert_alpha()
+        self.Jump2GB=pygame.image.load('2JumpGB.png').convert_alpha()
+        self.image = self.baseGB
+        self.rect = self.image.get_rect()
+
+# initialisation des caracteristiques
+        self.change_x =0
+        self.change_y =0
+        self.vie=4
+        self.orientation=1
+        self.anim=0
+        self.tir=0
+        self.score=0
+
+
+
+
+
+    def update(self):
+        #actualisation de l'image
+        self.rect.x += self.change_x
+        self.rect.y += self.change_y
+        self.anim+=1
+        if self.rect.y<430:
+            self.change_y+=0.5
+
+        if self.rect.x<0:
+            self.rect.x-=self.change_x
+
+        if self.rect.x>1050:
+            self.rect.x-=self.change_x
+
+        if self.rect.y==430:
+            self.saut=0
+
+        if self.rect.y>430:
+            self.rect.y=430
+            self.change_y=0
+            self.saut=0
+
+        if self.orientation==0:
+
+            if self.change_x==0 and self.change_y==0:
+                self.image = self.baseGB
+            elif self.change_y==0 :
+                self.rect.y=455
+                if 0<self.anim and self.anim<10:
+                    self.image=self.Run1GB
+                if 10<=self.anim and self.anim<20:
+                    self.image=self.Run2GB
+                if 20<=self.anim and self.anim<30:
+                    self.image=self.Run3GB
+                if 30<=self.anim and self.anim<40:
+                    self.image=self.Run4GB
+                if 40<=self.anim and self.anim<50:
+                  self.image=self.Run5GB
+                if 50<=self.anim and self.anim<60:
+                   self.image=self.Run6GB
+                if self.anim>=60:
+
+                    self.anim=0
+            else :
+                if 0<self.anim and self.anim<40:
+                    self.image=self.Jump1GB
+                if 40<=self.anim and self.anim<80:
+                    self.image=self.Jump2GB
+
+                    self.anim=0
+
+
+
+
+
+        if self.orientation==1:
+
+            if self.change_x==0 and self.change_y==0:
+                self.image = self.baseDB
+            elif self.change_y==0 :
+                self.rect.y=455
+                if 0<self.anim and self.anim<10:
+                    self.image=self.Run1DB
+                if 10<=self.anim and self.anim<20:
+                    self.image=self.Run2DB
+                if 20<=self.anim and self.anim<30:
+                    self.image=self.Run3DB
+                if 30<=self.anim and self.anim<40:
+                    self.image=self.Run4DB
+                if 40<=self.anim and self.anim<50:
+                  self.image=self.Run5DB
+                if 50<=self.anim and self.anim<60:
+                   self.image=self.Run6DB
+                if self.anim>=60:
+                    self.anim=0
+
+
+
+            else :
+                if 0<self.anim and self.anim<40:
+                    self.image=self.Jump1DB
+                if 40<=self.anim and self.anim<80:
+                    self.image=self.Jump2DB
+
+                    self.anim=0
+
+
+# Controles du joueur
+
+    def mvtgauche(self):
+        """Mouvement gauche """
+        self.change_x = -3
+        self.change_y = 0
+        self.orientation=0
+
+    def mvtdroit(self):
+        """Mouvement droite"""
+        self.change_x = 3
+        self.change_y = 0
+        self.orientation=1
+
+    def jump(self):
+        if self.saut==0:
+            self.saut=1
+            self.change_y=-18
+        if self.orientation==0:
+            if self.change_y==0:
+                self.image = self.baseGB
+            else :
+                if 0<self.anim and self.anim<-2:
+                    self.image=self.Jump1GB
+                if self.anim>=-2:
+                    self.anim=0
+        if self.orientation==1:
+            if self.change_y==0:
+                self.image = self.baseDB
+            else :
+                if 0<self.anim and self.anim<-2:
+                    self.image=self.Jump1DB
+                if self.anim>=-2:
+                    self.anim=0
 
 
 
